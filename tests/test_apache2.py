0971b2d274f18d185ed9b190c65c276cb06b1098
Joshua Niemann <5431390+sp1kedshell@users.noreply.github.com>
2019-09-19 22:52:25 -0400

    Refactored previous branch docker-apache.  Added unit tests and docker apache server. TravisCI build passes.

---
+++ tests/test_apache2.py
@@ -0,0 +1,27 @@
+import sys, requests, pytest
+
+#eh, mildly disgusting, but it does work.  In the future use some kind of unique identifier to figure out if it's correct.
+teststr = 'b\'<!DOCTYPE html>\\n<html lang="en">\\n<head>\\n\\t<title>Project: Hello World</title>\\n</head>\\n<body>\\n\\t<h1>CSEC380: VulnApp</h1>\\n\\t<p>Hello World</p>\\n</body>\\n</html>\\n\''
+
+def checkIP(location):
+        try :
+                r = requests.get(location)
+                if(str(r.content) == teststr):
+                        return("Good")
+                else:
+                        return("Error: Server not operational")
+        except :
+                return("Network error.  Server down.")
+        
+        
+
+def main():
+        #assume localhost
+        ip = 'http://127.0.0.1:8080'
+        return checkIP(ip)
+
+     
+
+def test_server_online():
+    assert main() == "Good"
+
