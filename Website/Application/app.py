2a7bad8432e07f3cb89b03d3f363921934df20fc
Joshua Niemann <jspike397@users.noreply.github.com>
2019-11-09 19:22:08 -0500

    Added some work

--- Website/Application/app.py
+++ Website/Application/app.py
@@ -1,13 +1,49 @@
 from flask import Flask, request
+import mysql, hashlib
+from mysql import connector
+
+#CHANGEME
+StaticPath='/var/www/'
+StaticSalt = 'pL41qCFwnagJcZsM'
 
 app = Flask(__name__, static_url_path='')
 
+app.secret_key = os.urandom(24)
+#initialize database
+db_connector = mysql.conector.connect(
+    host="db",
+    user="root",
+    password="absolutely_totally_secure",
+    database="Application"
+)
+
+cursor = db_connector.cursor()
+
+def get_file(filename):
+    try:
+        src = StaticPath + filename
+        return open(src).read()
+    except IOError as exc:
+        return("No file file exists.  ya done fucked up.")
+
 @app.route("/")
 def root():
-    pass
+    content = get_file('index.html')
+    return Response(content, mimetype='text/html')
 
-@app.route("/upload")
-def upload():
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.get('user')
+    password = request.get('password')
+    hash = hashlib.sha3((StaticSalt + password).encode()).hexdigest()
+    #get hash from database
+    cursor.execute("SELECT password_hash_salt from User_Login WHERE Username like %s;", username)
+    result = cursor.fetchone()
+    if(str(result) == hash):
+        #redirect
+    pass
+@app.route("/landing")
+def landing():
     pass
 
 @app.route("/delete/<file>")
@@ -24,11 +60,13 @@ def logout():
 
 @app.route("/getSession")
 def getSession():
-    pass
+    if "username" in session:
+        return session
+    return None
 
 @app.route("/endSession")
 def endSession():
-    pass
-
+    session.pop("username", None)
+    
 if __name__ == "__main__":
     app.run()
