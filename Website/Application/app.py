b2dbac9525d529f49017e24c0489fd0282427db0
Connor Leavesley <connorleavesley1@gmail.com>
2019-11-09 20:00:08 -0500

    Added HTML files, added functionality to app.py

--- Website/Application/app.py
+++ Website/Application/app.py
@@ -1,34 +1,88 @@
-from flask import Flask, request
+from flask import Flask, request, session, render_template, url_for, redirect
+import mysql, hashlib
+from mysql import connector
+
+#CHANGEME
+StaticPath='/var/www/'
 
 app = Flask(__name__, static_url_path='')
+app.secret_key = os.urandom(24)
 
-@app.route("/")
+#initialize database
+db_connector = mysql.conector.connect(
+    host="db",
+    user="root",
+    password="absolutely_totally_secure",
+    database="Application"
+)
+
+cursor = db_connector.cursor()
+
+#
+def get_file(filename):
+    try:
+        src = StaticPath + filename
+        return open(src).read()
+    except IOError as exc:
+        return("No file file exists.")
+
+@app.route("/", methods=['GET'])
 def root():
+    return render_template('index.html')
+
+#
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.get('user')
+    password = request.get('password')
+
+    # Get the salt
+    cursor.execute("SELECT salt FROM User_Login WHERE Username LIKE %s;", username)
+    salt = cursor.fetchone()
+
+    # hash
+    hash = hashlib.sha3((salt + password).encode()).hexdigest()
+
+    # Get hash from database
+    cursor.execute("SELECT password_hash_salt FROM User_Login WHERE Username LIKE %s;", username)
+    result = cursor.fetchone()
+
+    if(str(result) == hash):
+        session["username"] = username
+        return redirect(url_for("landing"))
+    else:
+        error = "Invalid Credentials"
+        return render_template('index.html', error=error)
+
+#
+@app.route("/landing")
+def landing():
     pass
 
-@app.route("/upload")
-def upload():
-    pass
-
+#
 @app.route("/delete/<file>")
 def delete(file):
     pass
 
+#
 @app.route("/video/<file>")
 def video(file):
-    pass
+    if "username" in session:
+        return render_template()
 
+#
 @app.route("/logout")
 def logout():
-    pass
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
+#
 @app.route("/getSession")
 def getSession():
-    pass
+    if "username" in session:
+        return session
+    return None
 
-@app.route("/endSession")
-def endSession():
-    pass
-
+#
 if __name__ == "__main__":
     app.run()
--- Website/Database/initialize_db.sql
+++ Website/Database/initialize_db.sql
@@ -6,7 +6,8 @@ use Application;
 CREATE TABLE User_Login
 (
 	Username VARCHAR(255),
-	password_hash_salt VARBINARY(64),
+    salt CHAR(16),
+	password_hash_salt CHAR(56),
 	PRIMARY KEY(Username)
 );
 CREATE TABLE Video_files
@@ -18,3 +19,4 @@ CREATE TABLE Video_files
 	PRIMARY KEY(Video_ID)
 );
 
+INSERT INTO User_Login (Username, salt, password_hash_salt) VALUES ("chaim", "pL41qCFwnagJcZsM", "f2094e7e7ce265f17fea0b8910206d9487ef37c0ee5fb8a676a42484")
