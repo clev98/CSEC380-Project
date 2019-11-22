b4da854d7381518c8d06af2ac9eb6f77912ec86b
Connor Leavesley <connorleavesley1@gmail.com>
2019-11-22 15:30:13 -0500

    one more

--- Tests/test_SQL_injection.py
+++ Tests/test_SQL_injection.py
@@ -4,7 +4,7 @@ def testClassic():
     s = requests.Session()
     r = s.post("https://localhost/login", data={'username' : "' OR 'x'='x", 'password': 'password'}, verify=False)
     s.close()
-    if "('pL41qCFwnagJcZsM', '8c0f5ab2947a5a03c5275599e8b1cff2f7f281d07702a4082fcafa239b621ad8', 'chaim')" in r.content:
+    if b"('pL41qCFwnagJcZsM', '8c0f5ab2947a5a03c5275599e8b1cff2f7f281d07702a4082fcafa239b621ad8', 'chaim')" in r.content:
         return True
     return False
 
