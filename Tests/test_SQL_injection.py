f0040063f2a44938e4e5ede8afd77dcfc3cf4fa5
Connor Leavesley <connorleavesley1@gmail.com>
2019-11-22 15:20:14 -0500

    Fixed test

--- Tests/test_SQL_injection.py
+++ Tests/test_SQL_injection.py
@@ -2,7 +2,7 @@ import sys, requests, pytest
 
 def testClassic():
     s = requests.Session()
-    r = s.post("http://localhost:5000/login", data={'username' : "' OR 'x'='x", 'password': 'password'})
+    r = s.post("https://localhost/login", data={'username' : "' OR 'x'='x", 'password': 'password'})
     s.close()
     if "('pL41qCFwnagJcZsM', '8c0f5ab2947a5a03c5275599e8b1cff2f7f281d07702a4082fcafa239b621ad8', 'chaim')" in r.content:
         return True
